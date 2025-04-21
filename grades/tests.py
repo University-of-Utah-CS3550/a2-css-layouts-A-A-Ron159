3fbb378e9fa47c6ebe8824d0fa6825d8d81361d1
aaron <aaronperez159@yahoo.com>
2025-04-21 12:04:41 -0600

    Added Tests

--- grades/tests.py
+++ grades/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test import Client
+from .models import Assignment, Submission
 
 # Create your tests here.
+class GradingAppTests(TestCase):
+    def setUp(self):
+        return super().setUp()
+    
+    def test_login_page(self):
+        client = Client()
+        response = client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Login" in str(response.content))
